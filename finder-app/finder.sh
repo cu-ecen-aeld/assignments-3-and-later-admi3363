f3b2152a598255534a382d9d20e186ce575fb5d3
Adam <atom@atom.vm>
2022-08-28 16:20:57 -0700

    initial push

---
+++ finder-app/finder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Accepts the following runtime arguments: the first argument is a path to a directory on the filesystem, referred to below as filesdir; 
+#the second argument is a text string which will be searched within these files, referred to below as searchstr
+filesdir=$1;
+searchstr=$2;
+
+#Exits with return value 1 error and print statements if any of the parameters above were not specified
+if [ $# -lt 2 ] 
+then
+    echo "Incorrect number of input params"
+    exit 1
+fi
+
+#set default vars
+numberOfFiles=0
+numberOfMatchingLines=0
+
+#Exits with return value 1 error and print statements if filesdir does not represent a directory on the filesystem
+if [ -d "$filesdir" ]
+then
+    #change to requested directory to be able to loop through files
+    cd ${filesdir};
+
+    #loop through all the files in this directory and look for the search string(param 2)
+    echo "${filesdir} exists"
+else
+    echo "Directory [${filesdir}] does not match a known directory"
+    exit 1
+fi
+
+#Prints a message "The number of files are X and the number of matching lines are Y" 
+#where X is the number of files in the directory and all subdirectories 
+#and Y is the number of matching lines found in respective files.
+echo "The number of files are ${numberOfFiles} and the number of matching lines are ${numberOfMatchingLines}"
---
+++ finder-app/writer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Accepts the following arguments: the first argument is a full path to a file (including filename) on the filesystem, referred to below as writefile; 
+#the second argument is a text string which will be written within this file, referred to below as writestr
+writefile=$1;
+writestr=$2;
+
+#Exits with return value 1 error and print statements if any of the parameters above were not specified
+if [ $# -lt 2 ] 
+then
+    echo "Incorrect number of input params"
+    exit 1
+fi
+
+#Creates a new file with name and path writefile with content writestr, overwriting any existing file and creating the path if it doesn’t exist. 
+#Exits with value 1 and error print statement if the file could not be created.
+mkdir ${writefile}
